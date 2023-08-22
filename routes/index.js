fc58c9bca6bf17afd0ec6013258eb652fdacfcf3
Igor Shidlovskiy <IShidlovskiy@alfabank.ru>
2023-08-22 16:14:45 +0300

    feat(*): сервер собранный на уроке

---
+++ routes/index.js
@@ -0,0 +1,53 @@
+const { getUsers } = require('../handlers/get-users');
+
+module.exports.routes = [
+  {
+    method: 'GET',
+    path: '/',
+    handler: (request, h) => {
+      return h.file('login.html');
+    },
+  },
+  {
+    method: 'GET',
+    path: '/params/{name}',
+    handler: (request, h) => {
+      return `<h1>Привет ${request.params.name}</h1>`;
+    },
+  },
+  {
+    method: 'GET',
+    path: '/query{name?}',
+    handler: (request, h) => {
+      return `<h1>Привет ${request.query.name}</h1>`;
+    },
+  },
+  {
+    method: 'GET',
+    path: '/{any*}',
+    handler: (request, h) => {
+      return `<h1>OOps! 404</h1>`;
+    },
+  },
+  {
+    method: 'GET',
+    path: '/file',
+    handler: getUsers,
+  },
+  {
+    method: 'POST',
+    path: '/login',
+    handler: (request, h) => {
+      const { username, password } = request.payload;
+      const data = {
+        username,
+        password,
+      };
+      if (password === '123') {
+        return h.view('index', data);
+      } else {
+        return h.redirect('/');
+      }
+    },
+  },
+];
